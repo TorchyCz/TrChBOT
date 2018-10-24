99b1d68260abf3048f746ccb8388bc3282399b6b
KlukCZ <41321902+KlukCZ@users.noreply.github.com>
2018-10-24 19:22:16 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,20 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+  let helpEmbed = new Discord.RichEmbed()
+  .setDescription("Pomoc")
+  .setColor("#100ba0")
+  .addField("!help", "Zobrazí tuto pomocnou zprávu")
+  .addField("!serverinfo", "Zobrazí informace o serveru")
+  .addField("!report", "Nahlásí hráče")
+  .addField("!kick", "Vyhodí hráče z Discord serveru")
+  .addField("!ban", "Zabanuje hráče z Discord serveru")
+  .addField("!usage", "Zobrazí jak použít příkazy")
+
+  message.channel.send(helpEmbed);
+  return;
+}
+
+module.exports.help = {
+  name: "help"
+}
