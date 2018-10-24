7157524b23e4abcb9d4edb220b7877fb1a8fca74
KlukCZ <41321902+KlukCZ@users.noreply.github.com>
2018-10-24 11:28:22 +0200

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,1 +1,17 @@
+const Duiscord = require("discord.js");
 
+module.exports.run = async => {
+  
+  //!hello
+  
+  let embed = new Discord.RichEmbed()
+  .setDescription("Hello")
+  .setColor("#bc0000")
+  .addField("Hello", `${messafge.author}`);
+  
+  message.channel.send(embed);
+  return;
+}
+module.exports.help = {
+  name: "hello"
+}
